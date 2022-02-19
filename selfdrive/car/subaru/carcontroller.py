e98bd0d8844d9f2f7a2ce526822101c4a9d0e6cd
Martin Lillepuu <martin@mlp.ee>
2022-02-19 17:09:20 +0200

    remove self.params

--- selfdrive/car/subaru/carcontroller.py
+++ selfdrive/car/subaru/carcontroller.py
@@ -39,7 +39,6 @@ class CarController():
 
     self.p = CarControllerParams(CP)
     self.packer = CANPacker(DBC[CP.carFingerprint]['pt'])
-    self.params = CarControllerParams()
 
   def update(self, c, enabled, CS, frame, actuators, pcm_cancel_cmd, visual_alert, left_line, right_line, left_lane_depart, right_lane_depart, lead_visible):
 
