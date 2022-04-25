a092215e75c0ae84bd5f1b4cbe83ab39629368b9
Martin Lillepuu <martin@mlp.ee>
2022-04-25 12:34:55 +0300

    Add Subaru Legacy 2020+ to test routes non tested cars

--- selfdrive/car/tests/routes.py
+++ selfdrive/car/tests/routes.py
@@ -30,6 +30,7 @@ non_tested_cars = [
   SUBARU.LEVORG_PREGLOBAL,
   SUBARU.WRX_PREGLOBAL,
   SUBARU.OUTBACK,
+  SUBARU.LEGACY,
 ]
 
 TestRoute = namedtuple('TestRoute', ['route', 'car_fingerprint', 'segment'], defaults=(None,))
