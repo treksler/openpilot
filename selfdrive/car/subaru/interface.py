7e382d9a7362f20be548e32cc9fc008e8e8d3b50
Martin Lillepuu <martin@mlp.ee>
2022-08-15 02:28:26 +0300

    Add pid tuning for Crosstrek

--- selfdrive/car/subaru/interface.py
+++ selfdrive/car/subaru/interface.py
@@ -85,6 +85,7 @@ class CarInterface(CarInterfaceBase):
       ret.centerToFront = ret.wheelbase * 0.5
       ret.steerRatio = 15  # spec = 13
       ret.steerActuatorDelay = 0.4   # end-to-end angle controller
+      ret.lateralTuning.init('pid')
       ret.lateralTuning.pid.kf = 0.00005
       ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kpBP = [[0., 20.], [0., 20.]]
       ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2, 0.3], [0.02, 0.03]]
